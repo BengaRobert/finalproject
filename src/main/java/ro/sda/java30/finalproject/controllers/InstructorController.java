fc8fdd31528cb72500fe9970d673826f813c6229
Lorena Rotaru <lorenarotaru2610@yahoo.com>
2022-04-09 15:59:28 +0300

    Added post method in InstructorController

--- src/main/java/ro/sda/java30/finalproject/controllers/InstructorController.java
+++ src/main/java/ro/sda/java30/finalproject/controllers/InstructorController.java
@@ -1,19 +1,37 @@
 package ro.sda.java30.finalproject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ro.sda.java30.finalproject.config.entities.Instructors;
-import ro.sda.java30.finalproject.languages.Languages;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ro.sda.java30.finalproject.entities.Instructors;
+import ro.sda.java30.finalproject.repository.InstructorRepository;
 
 @Controller
 public class InstructorController {
 
-    public InstructorController() {
+    @Autowired
+    private final InstructorRepository instructorRepository;
+
+    public InstructorController(InstructorRepository instructorRepository) {
+        this.instructorRepository = instructorRepository;
     }
+
+
     @GetMapping("/instructor")
     public String showInstructors(Model model){
         model.addAttribute("formObject", new Instructors());
        return "instructor";
     }
+
+    @PostMapping("/instructor")
+    public String saveAllInstructors(@ModelAttribute("formObject") Instructors form, Model model){
+        instructorRepository.save(form);
+        return "instructor";
+    }
+
+
 }
