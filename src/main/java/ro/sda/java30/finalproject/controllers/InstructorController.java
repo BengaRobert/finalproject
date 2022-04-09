d03272ade0c2b1cace06164259094e2804c8f94b
Negru Bogdan <bogdan1994_2006@yahoo.com>
2022-04-09 15:37:51 +0300

    Added Languages

--- src/main/java/ro/sda/java30/finalproject/controllers/InstructorController.java
+++ src/main/java/ro/sda/java30/finalproject/controllers/InstructorController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ro.sda.java30.finalproject.config.entities.Instructors;
+import ro.sda.java30.finalproject.languages.Languages;
 
 @Controller
 public class InstructorController {
---
+++ src/main/java/ro/sda/java30/finalproject/languages/Languages.java
@@ -0,0 +1,4 @@
+package ro.sda.java30.finalproject.languages;
+
+public enum Languages { ENGLEZA , FRANCEZA , ROMANA , SPANIOLA , ITALIANA , GERMANA
+}
